ae2545dc59e96f1b16c89ac91384af3c796859c1
Csaba Barcsa <mouseofblind@gmail.com>
2022-09-03 21:19:17 +0200

    Clearing paths

--- system/sys.php
+++ system/sys.php
@@ -1,22 +1,14 @@
 <?php
 defined('ISRUN') OR exit('Direct access to the script not allowed!');
 
-//defined('') OR 
+//defined('') OR
 defined('EXAMPPATH') OR define('EXAMPPATH', SYSPATH.DS.'Examp');
-defined('APPPATH') OR define('APPPATH', BASEPATH.DS.'app');
-define('VIEWSPATH', APPPATH.DS.'views');
-define('CTRLSPATH', APPPATH.DS.'controllers');
-define('MDLSPATH', APPPATH.DS.'models');
-
-define('COREPATH', EXAMPPATH.DS.'Core');
 define('SUPPORTPATH', EXAMPPATH.DS.'support');
-define('HANDLRPATH', COREPATH.DS.'Handlers');
 
 if(ENV == 'development' && file_exists(SUPPORTPATH.DS.'helpers'.DS.'dev_helpr.php'))
 {
     require_once SUPPORTPATH.DS.'helpers'.DS.'dev_helpr.php';
 }
-
 
 if(file_exists(SUPPORTPATH.DS.'autoloaders.php'))
 {
